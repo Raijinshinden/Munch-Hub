589eb19f35147abfcd7037ef2904b44713b26ff6
Ryan Dempsey <rjd9896@gmail.com>
2024-08-15 00:29:13 +0900

    Update Navbar.jsx

--- Munch-Hub/src/Components/Navbar.jsx
+++ Munch-Hub/src/Components/Navbar.jsx
@@ -3,7 +3,9 @@ import React, {useState} from 'react'
 function Navbar() {
 const [nav, setNav] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+
+    </div>
   )
 }
 
