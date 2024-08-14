29d1cff492c876857e4cc53336a36e34d53b39e0
Ryan Dempsey <rjd9896@gmail.com>
2024-08-15 00:33:16 +0900

    Update Navbar.jsx

--- Munch-Hub/src/Components/Navbar.jsx
+++ Munch-Hub/src/Components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React, {useState} from 'react'
+import {AiFillTag, AiOutlineClose, AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai'
 
 function Navbar() {
 const [nav, setNav] = useState(false)
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+        {/* Left side */}
+        <div className='flex items-center'>
+            <div onClick={()=> setNav(!nav)} className='cursor-pointer'>
+                <AiOutlineMenu size={30}/>
+            </div>
+
+        </div>
 
     </div>
   )
