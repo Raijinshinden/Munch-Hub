459927482a50eb64d62417f9b3c7eb211dc0f636
Ryan Dempsey <rjd9896@gmail.com>
2024-08-15 23:41:31 +0900

    Update Navbar.jsx

--- Munch-Hub/src/Components/Navbar.jsx
+++ Munch-Hub/src/Components/Navbar.jsx
@@ -23,6 +23,7 @@ const [nav, setNav] = useState(false)
         {/* Search Input */}
 
         <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+            <AiOutlineSearch size={20}/>
 
         </div>
 
