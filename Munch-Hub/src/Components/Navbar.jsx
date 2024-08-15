30b2ba0504cb20f79be9283ac35b1143c2c683ad
Ryan Dempsey <rjd9896@gmail.com>
2024-08-16 00:16:07 +0900

    Update Navbar.jsx

--- Munch-Hub/src/Components/Navbar.jsx
+++ Munch-Hub/src/Components/Navbar.jsx
@@ -39,7 +39,7 @@ const [nav, setNav] = useState(false)
 
 
         {/* Side drawer menu */}
-        <div className=''>
+        <div className={nav ? 'fixed top-0 left-0 w-[300px]'}>
 
         </div>
 
