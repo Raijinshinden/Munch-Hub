307f56642f33260952b2e6c682dd75a09dcf8807
Ryan Dempsey <rjd9896@gmail.com>
2024-08-17 00:35:14 +0900

    Update Hero.jsx

--- Munch-Hub/src/Components/Hero.jsx
+++ Munch-Hub/src/Components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
             {/* Overlay */}
             <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
-                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Food</span>Deals</h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Meal</span>Deals</h1>
             </div>
             <img className='w-full max-h-[500px] object-cover' src=''/>
         </div>
