c620b2590b8e8acda211d0c1d82ec4ef19cf9f1c
Ryan Dempsey <rjd9896@gmail.com>
2024-08-17 00:14:58 +0900

    Update Hero.jsx

--- Munch-Hub/src/Components/Hero.jsx
+++ Munch-Hub/src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     <div className='max-w-[1640px] mx-auto p-4'>
-        <div className=''>
+        <div className='max-h-[500px] relative'>
 
         </div>
     </div>
