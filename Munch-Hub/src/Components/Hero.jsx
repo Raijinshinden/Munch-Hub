02979ab6cbafda99a75fc1b6dfb676cd33ae887c
Ryan Dempsey <rjd9896@gmail.com>
2024-08-17 00:38:34 +0900

    Update Hero.jsx

--- Munch-Hub/src/Components/Hero.jsx
+++ Munch-Hub/src/Components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Meal</span>Deals</h1>
             </div>
-            <img className='w-full max-h-[500px] object-cover' src=''/>
+            <img className='w-full max-h-[500px] object-cover' src='https://images.pexels.com/photos/8753672/pexels-photo-8753672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
         </div>
     </div>
   )
