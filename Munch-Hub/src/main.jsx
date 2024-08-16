bc796c36b03c40d24a50e4d0d41d51212177dc9b
Ryan Dempsey <rjd9896@gmail.com>
2024-08-17 00:12:11 +0900

    Created a Hero Component

--- Munch-Hub/src/main.jsx
+++ Munch-Hub/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Hero from './Components/Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Hero/>
   </StrictMode>,
 )
