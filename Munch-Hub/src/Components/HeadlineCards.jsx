5abc9b87a118668f0ed5530661aca880778dceb9
Ryan Dempsey <rjd9896@gmail.com>
2024-08-17 23:56:37 +0900

    Update HeadlineCards.jsx

--- Munch-Hub/src/Components/HeadlineCards.jsx
+++ Munch-Hub/src/Components/HeadlineCards.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function HeadlineCards() {
   return (
-    <div></div>
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        <div className='rounded-xl relative'> {/* Card 1 */}
+            {/* Card Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+
+            </div>
+
+        </div>
+    </div>
   )
 }
 
