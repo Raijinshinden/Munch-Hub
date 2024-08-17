7bca1f6bbba10c958e7eb5c0922ba65a28a90f87
Ryan Dempsey <rjd9896@gmail.com>
2024-08-18 00:00:52 +0900

    Update HeadlineCards.jsx

--- Munch-Hub/src/Components/HeadlineCards.jsx
+++ Munch-Hub/src/Components/HeadlineCards.jsx
@@ -6,8 +6,9 @@ function HeadlineCards() {
         <div className='rounded-xl relative'> {/* Card 1 */}
             {/* Card Overlay */}
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                <p className='font-bold text-2xl px-2 pt-2 '>Buy one</p>
-
+                <p className='font-bold text-2xl px-2 pt-2 '>Buy One, Get One Free deals</p>
+                <p className='px-2'>Updated Weekly</p>
+                <button className='border-white bg-white'></button>
             </div>
 
         </div>
