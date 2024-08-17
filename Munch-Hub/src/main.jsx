6eb6016a2f9746b25369c9e2af06e186c3668643
Ryan Dempsey <rjd9896@gmail.com>
2024-08-17 23:48:46 +0900

    Update main.jsx

--- Munch-Hub/src/main.jsx
+++ Munch-Hub/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Hero from './Components/Hero.jsx'
-import HeadlineCards from './Components/HeadlineCards.jsx'
 import './index.css'
+import Hero from './Components/Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Hero/>
-    <HeadlineCards/>
+    
   </StrictMode>,
 )
