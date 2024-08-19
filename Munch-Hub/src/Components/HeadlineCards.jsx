9717f1ff796a61190cb13d9d2580ec35c0e9e75d
Ryan Dempsey <rjd9896@gmail.com>
2024-08-19 23:54:07 +0900

    Update HeadlineCards.jsx

--- Munch-Hub/src/Components/HeadlineCards.jsx
+++ Munch-Hub/src/Components/HeadlineCards.jsx
@@ -9,7 +9,7 @@ function HeadlineCards() {
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
                 <p className='font-bold text-2xl px-2 pt-2 '>Buy One, Get One Free deals</p>
                 <p className='px-2'>Updated Weekly</p>
-                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>See more</button>
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
                  src='https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
@@ -21,7 +21,7 @@ function HeadlineCards() {
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
                 <p className='font-bold text-2xl px-2 pt-2'>New Restaurants</p>
                 <p className='px-2'>Added Daily</p>
-                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Learn More</button>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>See More</button>
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
                  src='https://images.pexels.com/photos/1267321/pexels-photo-1267321.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
