26c66ee8e208d8e86bdb852bbc68ece4f9346b5c
Ryan Dempsey <rjd9896@gmail.com>
2024-08-21 00:03:48 +0900

    Update Foods.jsx

--- Munch-Hub/src/Components/Foods.jsx
+++ Munch-Hub/src/Components/Foods.jsx
@@ -9,7 +9,7 @@ function Foods() {
   const filterType = (category) => {
     setFoods(
         data.filter((item) => {
-            return item.category === category
+            return item.category === category;
         })
     )
   }
