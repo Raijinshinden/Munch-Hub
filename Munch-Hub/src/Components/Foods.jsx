3809d70ef21bd5aa97db2dbb493d48843acfa1a8
Ryan Dempsey <rjd9896@gmail.com>
2024-08-21 00:01:36 +0900

    Update Foods.jsx

--- Munch-Hub/src/Components/Foods.jsx
+++ Munch-Hub/src/Components/Foods.jsx
@@ -3,7 +3,10 @@ import React, { useState } from 'react'
 function Foods() {
   const Food = () => {
   
-  const [foods, setFoods] = useState(data)
+  const [foods, setFoods] = useState(data);
+
+  // Filter by Type burgers/pizza/etc...
+  const filterType = ()
   }
 }
 
