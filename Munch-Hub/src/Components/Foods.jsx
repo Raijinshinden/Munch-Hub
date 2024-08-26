63686e3a861cf3e09d4ba659bdb91c45afe53ba7
Ryan Dempsey <rjd9896@gmail.com>
2024-08-27 00:01:06 +0900

    Update Foods.jsx

--- Munch-Hub/src/Components/Foods.jsx
+++ Munch-Hub/src/Components/Foods.jsx
@@ -72,7 +72,9 @@ import React, { useState } from 'react'
         {/* Display Foods */}
         <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
             {foods.map((item, index)=>(
-                
+                <div>
+                    
+                </div>
             ))}
 
         </div>
