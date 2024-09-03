2093896e4db4b7e8181477d236ca503f0dffadb0
Ryan Dempsey <rjd9896@gmail.com>
2024-09-03 23:56:38 +0900

    Update data.js

--- Munch-Hub/src/data/data.js
+++ Munch-Hub/src/data/data.js
@@ -64,7 +64,7 @@ export const data = [
     },
     {
         id: 10,
-        name: 'pork vindaloo',
+        name: 'Pork Vindaloo',
         category: 'curry',
         image: '',
         price: '$$',
