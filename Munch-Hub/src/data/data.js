2f386889b8282036d0ddb3441f103ee26a8cdcd2
Ryan Dempsey <rjd9896@gmail.com>
2024-09-03 23:56:48 +0900

    Update data.js

--- Munch-Hub/src/data/data.js
+++ Munch-Hub/src/data/data.js
@@ -71,7 +71,7 @@ export const data = [
     },
     {
         id: 11,
-        name: 'Japanese curry',
+        name: 'Japanese Curry',
         category: 'curry',
         image: '',
         price: '$',
