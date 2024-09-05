8cae2db5bf95e54d2bb94aaa742d8a42953ef40b
Ryan Dempsey <rjd9896@gmail.com>
2024-09-05 23:56:22 +0900

    Update Category.jsx

--- Munch-Hub/src/Components/Category.jsx
+++ Munch-Hub/src/Components/Category.jsx
@@ -4,7 +4,7 @@ import { categories } from '../data/data'
 const Category = () => {
     return (
         <div className='max-w-[1640px] m-auto px-4 py-12'>
-            <h1></h1>
+            <h1 className='text-yellow-500 font-bold text-4xl text-center'></h1>
 
         </div>
     )
