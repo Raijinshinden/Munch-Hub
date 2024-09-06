6624c470a62478517db155e4c8af999d9580a3bb
Ryan Dempsey <rjd9896@gmail.com>
2024-09-06 23:49:41 +0900

    Update Category.jsx

--- Munch-Hub/src/Components/Category.jsx
+++ Munch-Hub/src/Components/Category.jsx
@@ -6,6 +6,9 @@ const Category = () => {
         <div className='max-w-[1640px] m-auto px-4 py-12'>
             <h1 className='text-yellow-500 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
             {/* Categories */}
+            <div>
+                
+            </div>
 
         </div>
     )
