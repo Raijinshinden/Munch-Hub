8a9a1fcb08765b8ee425798f405118ea0a7ca429
Ryan Dempsey <rjd9896@gmail.com>
2024-09-07 23:58:21 +0900

    Update data.js

--- Munch-Hub/src/data/data.js
+++ Munch-Hub/src/data/data.js
@@ -39,7 +39,7 @@ export const data = [
         name: 'Fried Chicken',
         category: 'chicken',
         image: 'https://media.istockphoto.com/id/1207976129/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E3%83%86%E3%83%BC%E3%83%96%E3%83%AB%E3%81%AE%E4%B8%8A%E3%81%AB%E5%88%86%E9%9B%A2%E3%81%95%E3%82%8C%E3%81%9F%E7%99%BD%E3%81%84%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%81%AB%E3%83%95%E3%83%A9%E3%82%A4%E3%83%89%E3%83%81%E3%82%AD%E3%83%B3%E3%82%92%E9%96%89%E3%81%98%E3%81%BE%E3%81%99%E3%81%8A%E3%81%84%E3%81%97%E3%81%84%E3%81%A8%E3%82%A4%E3%82%A8%E3%83%AD%E3%83%BC%E3%82%B4%E3%83%BC%E3%83%AB%E3%83%89%E3%81%AE%E8%89%B2%E3%82%92%E8%A6%8B%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84.jpg?s=2048x2048&w=is&k=20&c=b6OfganvAdQQ1Lu__yLFAE7M82pyc47hsX2Xev_N9w8=',
-        price: '$$',
+        price: '$10',
     },
     {
         id: 7,
