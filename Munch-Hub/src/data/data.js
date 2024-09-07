7a0c59156c1fe8b20b50f0872ea29acba98b85fa
Ryan Dempsey <rjd9896@gmail.com>
2024-09-07 23:48:25 +0900

    Update data.js

--- Munch-Hub/src/data/data.js
+++ Munch-Hub/src/data/data.js
@@ -189,6 +189,6 @@ export const categories = [
         id: 8,
         name: 'Shipped Free',
         image:
-            '',
+            'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
     },
 ];
